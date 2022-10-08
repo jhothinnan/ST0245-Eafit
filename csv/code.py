bf482876c77849531de7307bf6100e8414ff0466
jhonnathan ocampo <jhonnathanocampo118@gmail.com>
2022-10-08 01:50:46 -0500

    Pruebas

--- csv/code.py
+++ csv/code.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import pprint
 
+#data = pd.read_csv('calles_de_medellin_con_acoso.csv', sep=';')
+
+#promedio = data['harassmentRisk'].mean()
+#data['harassmentRisk'] = data['harassmentRisk'].fillna(promedio)
+
+#unicas = data["origin"].unique()
+
+#grafo = {}
+#for i in unicas:
+#    grafo[i] = {}
+
+#for i in data.index:
+#    grafo[data["origin"][i]][data["destination"][i]] = (data["length"][i], data["harassmentRisk"][i])
+#    if data['oneway'][i] == True:
+#        grafo[data['destination'][i]] = {data["origin"][i]: (data["length"][i], data["harassmentRisk"][i])}
+
 data = pd.read_csv('calles_de_medellin_con_acoso.csv', sep=';')
 
 average = data['harassmentRisk'].mean()
@@ -16,5 +32,17 @@ for i in data.index:
     if data['oneway'][i] == True:
         graph[data['destination'][i]] = {data["origin"][i]: (data["length"][i], data["harassmentRisk"][i])}
 
-pprint.pprint(graph)
-pprint.pprint(data)
+
+
+
+# and data["destination"][i] not in grafo
+# print(graph)
+# print(len(graph))
+# pprint.pprint(graph)
+# print(data['origin'])
+# print(unicas)
+# print(data.columns.values)
+# print(data['harassmentRisk'])
+# print(data.loc[[1]])
+# print(data[['harassmentRisk']].to_string(index=False))
+# print(len(data["origin"].unique()))
